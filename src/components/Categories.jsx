52a6e7a5d3ffc9911fafdbfa37cc29349f32ba7e
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-05 23:20:33 +0530

    aj , first commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Carousel from 'carousel-react-rcdev'
 const Categories = () => {
   return (
     <div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'shelduckBlue' : '#98DAD9',
-      'AntiFlashWhite' : '#EAEBED',
-      'AirBlue' : '#5B8291',
-      'Charcoal' : '#2E424D',
-      'white': '#ffffff',
-      'black' : '#000000',
+    extend:{
+      colors: {
+        'shelduckBlue' : '#98DAD9',
+        'AntiFlashWhite' : '#EAEBED',
+        'AirBlue' : '#5B8291',
+        'Charcoal' : '#2E424D',
+        'white': '#ffffff',
+        'black' : '#000000',
+      },
     },
+    
   },
   plugins: [],
 }
