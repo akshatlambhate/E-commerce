ab6cae8517dae001bb4d2629805f4b89a9854417
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-10-08 23:41:05 +0530

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar.jsx';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Navbar />
     </div>
   );
