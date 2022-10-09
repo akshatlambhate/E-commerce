4de022ef4694f5f4f10121220c94a979513970fc
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-09 17:24:08 +0530

    aj-205, contact form first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar.jsx';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <ContactUs />
     </div>
   );
 }
