ca9eb8a28c60cb7ca807a5d8cea5e7d4bfd34f7d
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-09 21:40:09 +0530

    aj-204, about-us component implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'shelduckBlue' : '#98DAD9',
+        'AntiFlashWhite' : '#EAEBED',
+        'AirBlue' : '#5B8291',
+        'Charcoal' : '#2E424D',
+        'white': '#ffffff',
+        'black' : '#000000',
+      },
+    },
   },
   plugins: [],
 }
