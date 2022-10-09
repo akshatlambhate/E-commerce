0e4a8fb54aaa92a609ef804087d771636ee60f11
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-09 12:50:26 +0530

    aj-103, box-shadow updated

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,29 +10,29 @@ const Categories = () => {
   return (
     <>
     <div className='flex flex-row p-8 gap-8'>
-      <div className='flex flex-col justify-center items-center '>
-        <img src={laptop01} alt="images" className='w-60 p-3  h-33 rounded-lg border-2	border-AntiFlashWhite' />
-        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer'>Laptop</p>
+      <div className='flex flex-col justify-center items-center'>
+        <img src={laptop01} alt="images" className='w-60 p-3 shadow-md	  h-33 rounded-lg border-2	border-AntiFlashWhite' />
+        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer mt-2'>Laptop</p>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <img src={ha} alt="images" className='w-60 p-3 h-30 rounded-lg bg-AntiFlashWhite' />
-        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer'>Laptop</p>
+        <img src={ha} alt="images" className='w-60 p-3 h-30 shadow-md rounded-lg bg-AntiFlashWhite' />
+        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer mt-2'>Home Appliances</p>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <img src={headphns} alt="images" className='w-60 p-3 h-30 rounded-lg border-2	border-AntiFlashWhite' />
-        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer'>Laptop</p>
+        <img src={headphns} alt="images" className='w-60 p-3 h-30 shadow-md rounded-lg border-2	border-AntiFlashWhite' />
+        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer mt-2'>Headphones</p>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <img src={ipad} alt="images" className='w-60 p-3 h-30 rounded-lg bg-AntiFlashWhite' />
-        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer'>Laptop</p>
+        <img src={ipad} alt="images" className='w-60 p-3 h-30 shadow-md rounded-lg bg-AntiFlashWhite' />
+        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer mt-2'>Tablets</p>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <img src={mobile} alt="images" className='w-60 p-3 h-30 rounded-lg border-2	border-AntiFlashWhite' />
-        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer'>Laptop</p>
+        <img src={mobile} alt="images" className='w-60 p-3 h-30 shadow-md rounded-lg border-2	border-AntiFlashWhite' />
+        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer mt-2'>Mobiles</p>
       </div>
       <div className='flex flex-col justify-center items-center'>
-        <img src={smartW} alt="images" className='w-60 p-3 h-30 rounded-lg bg-AntiFlashWhite' />
-        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer'>Laptop</p>
+        <img src={smartW} alt="images" className='w-60 p-3 h-30 shadow-md rounded-lg bg-AntiFlashWhite' />
+        <p className='text-Charcoal text-[18px] font-bold hover:underline hover:cursor-pointer mt-2'>SmartWatch</p>
       </div>
       
     </div>
