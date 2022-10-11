3e218ee9c992fb460e2d5a698e6ab5f67ed62a49
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-12 00:01:46 +0530

    aj-107, subcomponent made

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import logoblack from '../assets/png/logo-img01.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebookSquare, faTwitterSquare, faInstagramSquare, faYoutubeSquare } from '@fortawesome/free-brands-svg-icons'
 import SubFooter from './SubFooter'
+import SocialMediaIcons from './SocialMediaIcons'
 const Footer = () => {
   return (
     <div class="	m-0 p-4	items-center w-[100%] bg-AntiFlashWhite">
@@ -12,10 +11,7 @@ const Footer = () => {
             <img src={logoblack} alt="Electronic Nest" class="w-40	"/>
           </div>
           <div class="flex gap-2	flex-row">
-            <FontAwesomeIcon icon={faFacebookSquare} class="w-10 h-10 text-blue-900 hover:text-blue-700" />
-            <FontAwesomeIcon icon={faTwitterSquare} class="w-10 h-10 text-blue-600 hover:text-blue-400" />
-            <FontAwesomeIcon icon={faInstagramSquare} class="w-10 h-10 text-rose-600 hover:text-rose-400" />
-            <FontAwesomeIcon icon={faYoutubeSquare} class="w-10 h-10 text-red-700 hover:text-red-500" />
+            <SocialMediaIcons />
           </div>
         </div>
         <div className='flex justify-between w-[70%]'>
