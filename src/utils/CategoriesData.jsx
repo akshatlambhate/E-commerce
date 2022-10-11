aaea67065b12105bc391f1209baa3f10f77c6090
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-11 22:15:11 +0530

    aj-103 , code refactored

---
+++ src/utils/CategoriesData.jsx
@@ -0,0 +1,33 @@
+import laptop01 from '../assets/laptop-03.png'
+import ha from '../assets/HA01.png'
+import headphns from '../assets/headPhns01.png'
+import ipad from '../assets/ipad.png'
+import mobile from '../assets/mobile.png'
+import smartW from '../assets/smartW.png'
+export const dataCat = [
+    {
+        'img':laptop01,
+        'tag':'Laptop',
+    },
+    {
+        'img':ha,
+        'tag':'Home Appliances',
+    },
+    {
+        'img':headphns,
+        'tag':'Headphones',
+    },
+    {
+        'img':ipad,
+        'tag':'Tablets',
+    },
+    {
+        'img':mobile,
+        'tag':'Mobiles',
+    },
+    {
+        'img':smartW,
+        'tag':'SmartWatch',
+    },
+    
+]
