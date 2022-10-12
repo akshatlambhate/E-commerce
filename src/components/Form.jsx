a975448965647e2b44fe8210c024618fd8f1f6b6
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-12 22:43:26 +0530

    aj-205, component updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Button from './Button'
-import FormFieldInput from './FormFieldInput'
-import FormFieldLabel from './FormFieldLabel'
 
 const Form = () => {
     return (
         <div>
             <form action="" className='flex flex-col text-left'>
-                <FormFieldLabel props={'Name'} />
-                <FormFieldInput />
-                <FormFieldLabel props={'Email'} />
-                <FormFieldInput />
-                <FormFieldLabel props={'Enquiry'} />
+                <label htmlFor="" className='text-sm text-AirBlue font-semibold	'>Name</label>
+                <input type="text" className='w-[70%] border-2 border-AntiFlashWhite focus:outline-none focus:border-AirBlue text-sm p-1 my-2' />
+                <label htmlFor="" className='text-sm text-AirBlue font-semibold	'>Email</label>
+                <input type="text" className='w-[70%] border-2 border-AntiFlashWhite focus:outline-none focus:border-AirBlue text-sm p-1 my-2' />
+                <label htmlFor="" className='text-sm text-AirBlue font-semibold	'>Enquiry</label>
                 <textarea name="enquiryText" id="" cols="30" rows="10" className='w-[70%] h-[100px] border-2 border-AntiFlashWhite focus:outline-none focus:border-AirBlue text-sm p-1 my-2'></textarea>
                 <Button props={'Submit!'} />
             </form>
