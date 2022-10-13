ea464335bc9de429df4585c7302c1b47cecb7402
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-13 22:54:04 +0530

    aj-204 , responsiveness added

--- src/components/SubAbout.jsx
+++ src/components/SubAbout.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import {dataAbout} from '../utils/DataAbout.jsx'
 const SubAbout = () => {
   return (
-      <div className='grid grid-cols-3 col-span-1 mt-8 mb-8'>
+      <div className='lg:grid lg:grid-cols-3  lg:mt-8 lg:mb-8 lg:gap-0
+      md:grid md:grid-cols-2 md:mt-4 md:mb-4 md:gap-2
+      xs:grid xs:grid-cols-1 xs:mt-2 xs:mb-2 xs:text-left'>
         {dataAbout.map((data)=>(
         <>
-        <div><img src={data.img} alt="" /></div>
-        <div className=' p-8 bg-AntiFlashWhite flex flex-col justify-center items-center'>
-          <h1 className='text-base text-AirBlue'>{data.heading}</h1>
-          <p className='text-sm text-AirBlue'>{data.para}</p>
+        <div><img src={data.img} alt="" /></div>  
+        <div className='
+        lg:p-8 bg-midSkyBlue lg:flex lg:flex-col lg:justify-center lg:items-center
+        md:p-4
+        xs:p-2  xs:flex xs:flex-col xs:justify-center xs:items-center xs:text-left'>
+          <h1 className='lg:text-base text-darkBlue
+          xs:text-sm'>{data.heading}</h1>
+          <p className='lg:text-sm text-darkBlue
+          xs:text-xs'>{data.para}</p>
         </div>
         </>
         ))}
