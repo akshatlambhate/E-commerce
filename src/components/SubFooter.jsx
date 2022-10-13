d8c234a28fa6b712fdd50fdd1d0cf96e0dddec88
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-13 15:03:55 +0530

    responsiveness checked

--- src/components/SubFooter.jsx
+++ src/components/SubFooter.jsx
@@ -5,7 +5,7 @@ const SubFooter = () => {
     <>
       {dataFoo.map((data)=>(
       <div className='text-left '>
-            <h1 class="xl:text-2xl text-AirBlue font-medium mb-4 hover:text-Charcoal xsm:text-base md:text-xl">{data.name}</h1>
+            <h1 class="xl:text-2xl text-AirBlue font-medium mb-4 hover:text-Charcoal sm:text-base md:text-xl">{data.name}</h1>
             <ul class=" text-AirBlue text-left text-base pb-2 hover:text-gray-600">
               <li>{data.item1}</li>
               <li>{data.item2}</li>
