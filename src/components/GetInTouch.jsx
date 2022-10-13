e66a0a4ab50e6bd1daac2f707841a3b5ce257bfa
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-13 22:13:19 +0530

    aj-106, btn updated

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -12,6 +12,7 @@ const GetInTouch = () => {
       xs:w-full ">
         <h1 class="lg:text-5xl lg:bold font-mono text-darkBlue
         lg:md-4
+        md:bold 
         md:mb-2
         xs:mb-2
         xs:text-base	xs:bold">Get In Touch!</h1>
@@ -24,7 +25,7 @@ const GetInTouch = () => {
         </p>
       </div>
       <div class="lg:flex lg:flex-row lg:gap-12 w-full items-center
-      xs:flex xs:flex-col xs:gap-6">
+      xs:flex xs:flex-col xs:gap-4">
       <input type="text" 
       placeholder='Enter your mail' 
       class="w-2/3
@@ -32,6 +33,9 @@ const GetInTouch = () => {
       py-1.5
       text-base
       font-normal
+      xs:px-1.5
+      xs:py-1
+      xs:text-sm
       text-darkBlue
       bg-lightSkyBlue bg-clip-padding
       border 
@@ -41,7 +45,8 @@ const GetInTouch = () => {
       focus:text-darkBlue focus:bg-white focus:border-AirBlue	focus:outline-none
       xs:w-full
       "/>
-      <button class="bg-blueShade	 hover:text-blueShade hover:bg-lightSkyBlue	 text-lightSkyBlue font-bold py-2 px-8 rounded-full">Subscribe</button>
+      <button class="bg-blueShade	 hover:text-blueShade hover:bg-lightSkyBlue	 text-lightSkyBlue font-bold lg:py-2 lg:px-8 rounded-full
+      xs:py-1 xs:px-4">Subscribe</button>
       </div>
     </div>
     <div className='md:items-center md:p-6 lg:p-4'>
