211f67982a5bf19cd8ffa2a447a175b43c7c2852
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-14 01:14:40 +0530

    responsiveness added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Button = ({props}) => {
   return (
     <>
-      <button className=' my-2 bg-AirBlue text-base text-AntiFlashWhite border-none rounded p-2 w-[20%] font-bold hover:text-AirBlue hover:bg-AntiFlashWhite'>{props}</button>
+      <button className=' lg:my-2 bg-darkBlue lg:text-base text-midSkyBlue hover:bg-midSkyBlue hover:text-darkBlue border-none rounded p-2 w-[20%] font-bold hover:text-AirBlue hover:bg-AntiFlashWhite
+      xs:my-1 xs:text-sm'>{props}</button>
     </>
   )
 }
