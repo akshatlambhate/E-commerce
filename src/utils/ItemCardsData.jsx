2e0849104e7574f155c71da6a5a6fbdcced220d7
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-10-13 20:07:30 +0530

    refactored code

---
+++ src/utils/ItemCardsData.jsx
@@ -0,0 +1,18 @@
+export const CardData =[
+    {
+    name:'Mobile Phone',
+    price: '$399',
+},
+    {
+    name:'Laptop',
+    price: '$455',
+},
+    {
+    name:'Mobile Phone',
+    price: '$399',
+},
+    {
+    name:'Mobile',
+    price: '$455',
+},
+]
