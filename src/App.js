320aa6b6ce06377ad022104c77221166f16483b3
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-10-14 19:15:34 +0530

    checkpoint01 merged

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import ContactUsPage from "./pages/ContactUsPage";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
-<<<<<<< HEAD
 import StorePage from "./pages/StorePage";
 import AboutPage from "./pages/AboutPage";
 import Login from "./pages/Login";
 import ItemCards from "./components/ItemCards";
 
-  
-=======
 import StorePage from './pages/StorePage';
 import Login from './pages/Login';
-import ProductView from "./components/ProductView";
+  import ProductView from "./components/ProductView";
 import ProductViewPage from "./pages/ProductViewPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
->>>>>>> d2185f7b223079b53357d32090e1e381d82887d6
 function App() {
   return (
-   
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}></Route>
--- src/components/SharedComponents/footer.jsx
+++ src/components/SharedComponents/footer.jsx
@@ -4,11 +4,6 @@ import SubFooter from '../SubFooter.jsx'
 import SocialMediaIcons from '../SocialMediaIcons.jsx'
 const Footer = () => {
   return (
-<<<<<<< HEAD
-    <div class="	m-0 p-4	items-center w-[100%] bg-background">
-      <div class="	flex gap-30	flex-row m-12 ">
-        <div class=" flex gap-12	flex-col w-[30%]">
-=======
     <div class="	m-0 p-4	items-center w-[100%] bg-lightSkyBlue">
       <div class="	lg:flex lg:gap-30	lg:flex-row m-8  
       xs:grid xs:grid-cols-2 xs:gap-4 xs:m-8
@@ -17,7 +12,6 @@ const Footer = () => {
         <div class=" flex gap-8	flex-col w-[60%] 
         xs:gap-2 xs:p-2 
         lg:w-[20%] lg:mr-16">
->>>>>>> d2185f7b223079b53357d32090e1e381d82887d6
           <div>
             <img src={logoblack} alt="Electronic Nest" class="w-40	lg:30"/>
           </div>
@@ -28,11 +22,7 @@ const Footer = () => {
         <SubFooter />
       </div>
       <div class="flex flex-row justify-center m-12">
-<<<<<<< HEAD
-      <h3 class="text-textcolor text-base">Copyright © 2022 Electronics Store</h3>
-=======
       <h3 class="text-blueShade lg:text-base xs:text-xs">Copyright © 2022 Electronics Store</h3>
->>>>>>> d2185f7b223079b53357d32090e1e381d82887d6
       </div>
     </div>
   )
