bbeef5cf204a89760f59b019e715494a8d254c8a
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-15 02:26:09 +0530

    updated

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ import { dataCat } from '../utils/CategoriesData.jsx'
 const Categories = () => {
   return (
     <>
-    <div className='lg:flex lg:flex-row lg:p-12 lg:gap-12 border-2 border-midSkyBlue w-[100%]
+    <div className='lg:flex lg:flex-row lg:p-12 lg:gap-12 border-b-2 border-parentdiv w-[100%]
     xs:grid xs:grid-cols-3 xs:grid-auto xs:gap-4 xs:p-8
     lg:h-[35vh]
     xs:h-[50vh] '>
@@ -12,7 +12,7 @@ const Categories = () => {
       '>
         <img src={data.img} alt="images" className=' lg:max-w-[50%] lg:p-2 lg:hover:max-w-[60%] xs:max-w-[100%] xs:p-2 xs:hover:max-w-[60%] ' 
         />
-        <p className='text-Charcoal lg:text-xl text-darkBlue font-bold hover:underline hover:cursor-pointer mt-2
+        <p className='text-Charcoal lg:text-xl text-colortxt font-semibold hover:underline hover:cursor-pointer mt-2
         xs:text-xs'>{data.tag}</p>
       </div>
     ))}
