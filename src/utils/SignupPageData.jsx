028c5f47bae899097cfdc719fa0a066b26f25597
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-10-14 12:55:17 +0530

    refactor code

---
+++ src/utils/SignupPageData.jsx
@@ -0,0 +1,33 @@
+ export const SignupData =[
+{
+    label:'FullName',
+    type:"text",
+    name:"fullname",
+    palceholder:"Enter Full Name"
+},
+{
+    label:'Email',
+    type:"email",
+    name:"email",
+    palceholder:"Enter Your Mail Address"
+},
+{
+    label:'Contact Number',
+    type:"number",
+    name:"contact",
+    palceholder:"Mobile Number"
+},
+{
+    label:'Address Detials',
+    type:"text",
+    name:"address",
+    palceholder:"Add your Address Details"
+},
+{
+    label:'Password',
+    type:"password",
+    name:"password",
+    palceholder:"Enter Password"
+},
+]
+
