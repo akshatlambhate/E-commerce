bd5438446945c7a5c56f1072cf961304397a14e5
Akshat Lambhate <akshatlambhate180302@gmail.com>
2022-10-14 09:37:43 +0530

    refactor code successfully

--- src/components/ItemCards.jsx
+++ src/components/ItemCards.jsx
@@ -13,13 +13,15 @@ const ItemCards = () => {
      <div id="items" className=' group flex flex-col gap-1 shadow-lg shadow-slate-600 items-center w-fit cursor-pointer'>
        <div><img src={data.pic} className="w-56"/>
         <div id='quick-view' className='-translate-y-6 bg-stone-700 text-white opacity-0 group-hover:opacity-100'>
-          <h1 className='hover:bg-black'>Quick View</h1>
+          <h1 className='hover:bg-black text-center'>Quick View</h1>
         </div></div>
         <div id="item-name" className='text-lg font-bold -mt-5 font-serif '>{data.name}</div>
         <div id="item-price" className='font-bold text-primary text-lg'>{data.price}</div>
         <div id="item-rating" className='mb-3'>
-          
-        
+        <FontAwesomeIcon icon={faStar} className="text-black hover:text-yellow-500 " />
+        <FontAwesomeIcon icon={faStar} className="text-black hover:text-yellow-500 " />
+        <FontAwesomeIcon icon={faStar} className="text-black hover:text-yellow-500 " />
+        <FontAwesomeIcon icon={faStar} className="text-black hover:text-yellow-500 " />
       </div>
       </div>
       ))}
