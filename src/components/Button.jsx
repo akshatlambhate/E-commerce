f9e515068c87269e1d0e48def17dde899cbc8b33
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-15 14:05:52 +0530

    aj,style added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({props}) => {
   return (
     <>
-      <button className=' lg:my-2 bg-darkBlue lg:text-base text-midSkyBlue hover:bg-midSkyBlue hover:text-darkBlue border-none rounded p-2 w-[20%] font-bold hover:text-AirBlue hover:bg-AntiFlashWhite
+      <button className=' lg:my-2 bg-buttoncolor lg:text-base text-buttontext hover:bg-buttoncolorhover hover:text-buttontexthover border-none rounded p-2 w-[20%] font-bold hover:text-AirBlue hover:bg-AntiFlashWhite
       xs:my-1 xs:text-sm'>{props}</button>
     </>
   )
