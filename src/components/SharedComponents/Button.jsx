628af087bd430028f40a8355fc1164d31fca0767
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-15 15:34:53 +0530

    aj,updated

---
+++ src/components/SharedComponents/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({props}) => {
+  return (
+    <>
+      <button className=' lg:my-2 bg-buttoncolor lg:text-base text-buttontext hover:bg-buttoncolorhover hover:text-buttontexthover border-none rounded p-2 w-[40%] font-bold hover:text-AirBlue hover:bg-AntiFlashWhite
+      xs:my-1 xs:text-sm'>{props}</button>
+    </>
+  )
+}
+
+export default Button
