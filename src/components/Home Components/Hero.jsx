11f9d091cd10a21a20506c16c63c95cf78108534
AakankshaJAngid <aakankshajangid929@gamil.com>
2022-10-19 22:20:30 +0530

    responsiveness added

--- src/components/Home Components/Hero.jsx
+++ src/components/Home Components/Hero.jsx
@@ -13,25 +13,26 @@ const Hero = () => {
       <Navbar />
       <div
         id="hero-text "
-        className="flex flex-col h-36"
-      ></div>
+        className="flex flex-col py-8"
+      >
       <div id="starting text">
         <h1 className="text-buttoncolor font-extrabold font-poppins lg:text-xl lg:ml-40 lg:mb-12 
-        xs:text-base xs:ml-10 xs:mb-6">
+        xs:text-base xs:ml-5 xs:mb-6">
           surrounded by technology
         </h1>
       </div>
       <div id="main text">
         <h1 className="text-buttoncolor font-semibold lg:text-6xl lg:ml-40 font-poppins 
-        xs:text-4xl xs:ml-10" style={{textShadow: '1px 1px 1px #fdf0d5'}}>
+        xs:text-4xl xs:ml-5" style={{textShadow: '1px 1px 1px #fdf0d5'}}>
           Get Awsome Deals on All <br></br>Electronic Products.
         </h1>
       </div>
       <div id="hero-button ">
         <button className="bg-buttoncolor lg:ml-40 px-5 lg:mt-5 py-2 lg:Smt-5 rounded-lg border-buttoncolorhover text-buttontext border-2 hover:bg-buttoncolorhover hover:text-buttoncolor 
-        xs:ml-10 xs:mt-3">
+        xs:ml-5 xs:mt-3">
           SHOP NEW TECH <FontAwesomeIcon icon={faArrowRight} />
         </button>
+      </div>
       </div>
     </div>
   );
